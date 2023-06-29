2e61e1354def3b1c9e340bc58edac77dac4ece9e
ChauTriTin <freuss47@gmail.com>
2023-06-29 18:03:49 +0700

    update

--- app/src/main/java/com/roy/starfield/MyApplication.kt
+++ app/src/main/java/com/roy/starfield/MyApplication.kt
@@ -2,12 +2,14 @@ package com.roy.starfield
 
 import android.app.Application
 
+//TODO permission ad_id
 //TODO ad
 //TODO keystore
 //TODO change icon launcher
 //TODO double to exit app
 //TODO policy
 //TODO rate app, share app, more app
+//TODO proguard
 
 //done
 
