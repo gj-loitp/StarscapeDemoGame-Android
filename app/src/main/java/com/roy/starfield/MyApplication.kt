6865b8f681e2071700ce58e3d35462cda5a487ff
ChauTriTin <freuss47@gmail.com>
2023-06-29 18:03:14 +0700

    update

---
+++ app/src/main/java/com/roy/starfield/MyApplication.kt
@@ -0,0 +1,18 @@
+package com.roy.starfield
+
+import android.app.Application
+
+//TODO ad
+//TODO keystore
+//TODO change icon launcher
+//TODO double to exit app
+//TODO policy
+//TODO rate app, share app, more app
+
+//done
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
